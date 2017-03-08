9e730d3f0c7244381bcc7e0fa8768edf16755668
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-03-07 23:33:54 -0500

    Added the first boiler plate batch to the application

---
+++ src/main/java/com/unips/UniPsApplication.java
@@ -0,0 +1,12 @@
+package com.unips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniPsApplication.class, args);
+	}
+}
