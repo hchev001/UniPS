22b3483eb04a856a9a06cc2160624555309fd4f1
Alfredo Lopez <alfredolopezf@hotmail.com>
2017-03-10 16:04:24 -0500

    Added a few more ant matchers for the security

--- src/main/java/com/unips/config/SecurityConfig.java
+++ src/main/java/com/unips/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic().and()
 			.authorizeRequests()
 				.antMatchers("/index.html", "/", "/login", "/message", "/home").permitAll()
+				.antMatchers("/api/businesses").hasAnyRole("BUSINESS", "ADMIN")
+				.antMatchers("/api/users").hasAnyRole("USER", "ADMIN")
 				.anyRequest().authenticated()
 				.and()
 			.csrf()
