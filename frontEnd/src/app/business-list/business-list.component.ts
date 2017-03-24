a943bd357d047cfc71534c57aca25aa1b5a73900
Hamilton Chevez <hchev001@fiu.edu>
2017-03-23 21:00:01 -0400

    changed the file name of the image component I think

--- frontEnd/src/app/business-list/business-list.component.ts
+++ frontEnd/src/app/business-list/business-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-business-list',
+  selector: 'business-list',
   templateUrl: './business-list.component.html',
   styleUrls: ['./business-list.component.css']
 })
