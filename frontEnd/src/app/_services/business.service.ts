1eb143c5f81f2ff9c4329677fb647a0e1f17d7c7
Hamilton Chevez <hchev001@fiu.edu>
2017-03-27 17:49:35 -0400

    created the template for business service and moved previously created models to _models folder

---
+++ frontEnd/src/app/_services/business.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http'
+
+@Injectable()
+export class BusinessService {
+
+  constructor() { }
+
+}
