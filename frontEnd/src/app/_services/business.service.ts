6d0d39c75b0d6be7edff2c55faefb3d05853f518
Hamilton Chevez <hchev001@fiu.edu>
2017-03-27 18:17:20 -0400

    created the getBusinesses api call from the client side

--- frontEnd/src/app/_services/business.service.ts
+++ frontEnd/src/app/_services/business.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http'
+import {Observable} from 'rxjs/Rx'
+
+//  Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class BusinessService {
 
-  constructor() { }
+private businessUrl = '/api/business'; // URL to web api
+  constructor(private _http: Http) { }
 
+  getBusinesses() {
+    return this._http.get(this.businessUrl)
+                      .map((res:Response) => res.json())
+                      .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+  }
 }
