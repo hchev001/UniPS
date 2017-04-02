cbdac45e5b23d23436dcc86e6ee3aac7e7893e4c
Hamilton Chevez <hchev001@fiu.edu>
2017-04-02 12:41:48 -0400

    Refactor business mmodel and service

--- frontEnd/src/app/_services/business.service.ts
+++ frontEnd/src/app/_services/business.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http'
 import {Observable} from 'rxjs/Rx'
+import { BusinessEntity } from '../_models/businessEntity.model';
 
 //  Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -9,12 +10,61 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class BusinessService {
 
-private businessUrl = '/api/business'; // URL to web api
+
+  private businessUrl = '/api/business'; // URL to web api
+
   constructor(private _http: Http) { }
 
   getBusinesses() {
     return this._http.get(this.businessUrl)
                       .map((res:Response) => res.json())
                       .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+  }
+
+  getBusinessesNew(): Observable<BusinessEntity> {
+    let listOfBiz = this._http
+      .get(this.businessUrl)
+      .map(this.mapBusinesses)
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+      return listOfBiz;
+  }
+
+  // Used to map each json entry to a bussiness entity model
+  mapBusinesses( response:Response): BusinessEntity[]{
+    return response.json().results.map(this.toBusiness);
+  }
+
+  mapBusiness(response:Response): BusinessEntity{
+    return this.toBusiness(response.json());
+  }
+
+
+  toBusiness(r:any): BusinessEntity{
+    let biz = <BusinessEntity>({
+      id: r.id,
+      createdDate: r.createdDate,
+      username: r.username,
+      password: r.password,
+      email: r.email,
+      question1: r.question1,
+      question2: r.question2,
+      pictureFeatured: r.pictureFeatured,
+      pictures: r.pictures,
+      description: r.description,
+      status: r.status,
+      role: r.role,
+      token: r.token,
+      name: r.name,
+      phone: r.phone,
+      phoneBusiness: r.phoneBusiness,
+      hours: r.hours,
+      rateAverage: r.rateAverage,
+      comments: r.comments,
+      ratings: r.ratings,
+      address: r.address,
+      category: r.category
+    });
+    console.log('Parsed business:', biz);
+    return biz;
   }
 }
--- frontEnd/src/app/search-field/search-field.component.ts
+++ frontEnd/src/app/search-field/search-field.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BusinessService } from '../_services/index';
+import { BusinessEntity} from '../_models/businessEntity.model';
 
 @Component({
   selector: 'app-search-field',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFieldComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bizService: BusinessService) {}
 
+businessList: BusinessEntity[] = [];
+bizList = {}
+
+getAllBiz() {
+  this.bizService.getBusinesses()
+                  .subscribe(data => this.businessList = data);
+  console.log(this.bizList);
+
+  // this.bizService.getBusinessesNew()
+  //                 .subscribe(data => this.biz = data);
+  // console.log(this.businessList);
+  // console.log(this.biz);
+}
   ngOnInit() {
   }
 
