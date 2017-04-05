60be62b733bc7b42077183c2b62ab0fab911f142
Hamilton Chevez <hchev001@fiu.edu>
2017-04-05 08:39:29 -0400

    Made succesfull single business GET api call
    Learned how to wait on the http call to response so that I don't get
    errors while loeading the page because the properties being interpolated
    are undefined. I interpolated business.name address.line1 address.city
    business.phoneBusiness, business.description

---
+++ frontEnd/src/app/_services/business.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http'
+import {Observable} from 'rxjs/Rx'
+import { BusinessEntity } from '../_models/businessEntity.model';
+import { Address } from '../_models/address.model';
+import { Comment } from '../_models/comment.model';
+
+//  Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class BusinessService {
+
+
+  private businessUrl = '/api/business'; // URL to web api
+
+  constructor(private _http: Http) { }
+  getBusinesses() {
+    return this._http.get(this.businessUrl)
+                      .map((res:Response) => res.json())
+                      .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+  }
+
+  getBusiness(username : string ) {
+      var Urladdress : string = "/api/business/" + username;
+      return this._http.get(Urladdress)
+                        .map((res:Response) => res.json())
+                        .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+
+  }
+
+}
