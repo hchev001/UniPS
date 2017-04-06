634b4f942aecf401f9882773005935d96444b80a
Alfredo Lopez <alfredolopezf@hotmail.com>
2017-04-06 11:36:57 -0400

    Added start.sh for macs and linux systems

--- start.cmd
+++ start.cmd
@@ -3,17 +3,17 @@
 set DIR=%~dp0
 
 
-:: 
 :: Build the Angular project
-::
-echo Building the frontend...
 cd ./frontEnd
+echo Installing dependencies
+call npm install
+
+echo Building the frontend...
 call ng build
 cd /d "%DIR%"
 
-::
+
 :: Clean the maven backend and rebuild
-::
 cd ./backend
 echo Cleaning the backend...
 call mvnw clean
@@ -21,10 +21,9 @@ echo Installing the backend...
 call mvnw install
 cd /d "%DIR%"
 
-::
 :: Start mysql
-:: 
-call "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqld"
+:: Not really a way to do it in the script
+echo Remember to start the MySql if not running.
 
 
 pause
