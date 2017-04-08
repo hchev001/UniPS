1a442a9b0ccf09bbe6de7ca4a100be29ad14e921
Hamilton Chevez <hchev001@fiu.edu>
2017-04-08 12:21:11 -0400

    added ReactiveForms to app.module and refactored user.service.ts

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { routes } from './app.router';
@@ -70,6 +70,7 @@ import { BusinessService } from './_services';
     BrowserModule,
     AlertModule.forRoot(),
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routes,
 
