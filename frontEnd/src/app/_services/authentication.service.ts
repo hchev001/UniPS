3efef8515aed34ae8242b6d078f8f5d655d9ad50
Hamilton Chevez <hchev001@fiu.edu>
2017-04-07 23:07:00 -0400

    refactored the login code

--- frontEnd/src/app/_services/authentication.service.ts
+++ frontEnd/src/app/_services/authentication.service.ts
@@ -1,7 +1,5 @@
-﻿//The authentication service is used to login and logout of the application,
-//to login it posts the users credentials to the api and checks the response for a token,
-//if there is one it means authentication was successful so the user details including the
-//token are added to local storage.
+﻿// The Authentication serivce is used to GET the user's login information credentials based
+﻿// on their username and password
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -11,37 +9,13 @@ import 'rxjs/add/operator/map'
 export class AuthenticationService {
     constructor(private http: Http) { }
 
-    login(username: string, password: string) {
-        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let user = response.json();
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                }
-            });
-    }
 
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-    }
+    authenticate(username:string, password:string) {
 
-    login2() {
-        var headers = {authorization : "Basic" + btoa("kathy:null")};
-        return this.http.get('/api/userInfo', headers )
-                        .map(this.extractData);
-    }
-
-    login3(username:string, password:string) {
-        // var headers = {authorization : "Basic" + btoa("kathy:k123
-        username = "kathy";
-        password = "k123";
-        var encodedCredentials: string = btoa('Basic '+username+':'+password);
+        var encodedCredentials: string = 'Basic ' + btoa(username+':'+password);
 
         let headers: Headers = new Headers
-        headers.append('X-XSRF-TOKEN', 'ff56ba5e-eb39-4999f-bcb8-e5a6b0dac267');
+
         headers.append('X-Requested-With', 'XMLHttpRequest');
         headers.append('authorization', encodedCredentials);
 
@@ -50,14 +24,5 @@ export class AuthenticationService {
 
         return this.http.get('/api/userInfo', opts )
                         .map(response => response.json());
-    }
-
-    private extractData(res: Response) {
-        let body;
-        if(res.text()) {
-            body = res.json();
-        }
-
-        return body || {};
     }
 }
--- frontEnd/src/app/login/login.component.ts
+++ frontEnd/src/app/login/login.component.ts
@@ -1,6 +1,4 @@
-/*The login component uses the authentication service to login and logout of the application.
-//It automatically logs the user out when it initializes (ngOnInit) so the login page can
-also be used to logout.*/
+
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlertService, AuthenticationService } from '../_services/index';
@@ -17,8 +15,6 @@ export class LoginComponent implements OnInit {
     model: any = {};
     loading = false;
     returnUrl: string;
-    model2: any = {};
-    model3
     data: Object;
     authenticated: boolean = false;
 
@@ -31,62 +27,28 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() {
         // reset login status
-        this.authenticationService.logout();
+
 
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-
-        this.alfredoLogin();
-        if (this.authenticated)
-            console.log("we've beeb authenticated")
-        else
-            console.log("we weren't authenticated")
     }
 
-    login() {
+        login() {
         this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
-
-    // alfredoLogin() {
-    //     // this.loading = true;
-    //     this.authenticationService.login3('kathy', 'k123')
-    //         .subscribe(data => {
-    //             if(data.name) {
-    //                 this.authenticated = true;
-    //                 this.data = data;
-    //             } else {
-    //                 this.authenticated = false;
-    //                 this.data = data;
-    //             }
-    //         });
-    // }
-    alfredoLogin() {
-        // this.loading = true;
-        var encodedCredentials: string = 'Basic ' + btoa('kathy:k123');
-
-        let headers: Headers = new Headers();
-        // headers.append('X-XSRF-TOKEN', 'ff56ba5e-eb39-4999f-bcb8-e5a6b0dac267');
-        headers.append('X-Requested-With', 'XMLHttpRequest');
-        headers.append('authorization', encodedCredentials);
-
-        let opts: RequestOptions = new RequestOptions
-        opts.headers = headers;
-
-        this.http.get('/api/userInfo', opts)
-            .subscribe((res:Response) => {
-              this.data = res.json();
-              this.authenticated = true;
-              console.log("Authenticated woot woot");
-            })
+        this.authenticationService.authenticate( this.model.username, this.model.password)
+        .subscribe(
+          (res:Response) => {
+            this.data = res;
+            this.authenticated = true;
+            console.log("Authenticated woot woot");
+          },
+          (err: any) => {
+            console.log(err);
+          },
+          () => {
+            console.log('no error and we are complete');
+          }
+        );
     }
 
 
