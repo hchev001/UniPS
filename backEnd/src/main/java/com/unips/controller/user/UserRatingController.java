970819378846b253261fe77b208fdc31bee6eb7d
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-04-09 20:44:18 -0400

    Added the Controller

--- backEnd/src/main/java/com/unips/controller/user/UserRatingController.java
+++ backEnd/src/main/java/com/unips/controller/user/UserRatingController.java
@@ -1,13 +1,17 @@
 package com.unips.controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unips.entity.Rating;
 import com.unips.entity.User;
 import com.unips.response.Response;
 import com.unips.service.UserService;
@@ -17,7 +21,7 @@ import io.swagger.annotations.ApiOperation;
 
 
 @RestController
-@RequestMapping("/api/users/{userName }/{businessName}/ratings")
+@RequestMapping("/api/users/{userName}/{businessName}/ratings")
 @Api(tags={"User-Rating"})
 public class UserRatingController {
 
@@ -28,22 +32,28 @@ public class UserRatingController {
 	@ApiOperation("Get rating for business")
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
-	public Response<User> getRating(@RequestBody User user) {
-		return Response.unimplemented();
+	public Response<Rating> getRating(
+			@RequestParam("userName") String userName,
+			@RequestParam("businessName") String businessName) {
+		return service.getRating(userName, userName);
 	}
 	
 	@ApiOperation("Add rating for business")
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
-	public Response<User> updateReview(@RequestBody User user){
-		return Response.unimplemented();
+	public Response<Rating> addReview(
+			@RequestParam("userName") String userName,
+			@RequestParam("businessName") String businessName){
+		return service.addRating(userName, businessName);
 	}
 	
 	
 	@ApiOperation("Update review by reviewId")
 	@RequestMapping(method = RequestMethod.PUT)
 	@ResponseBody
-	public Response<Integer> deleteReview(@PathVariable("username") String username){
-		return Response.unimplemented();
+	public Response<Rating> updateReview(
+			@RequestParam("userName") String userName,
+			@RequestParam("businessName") String businessName){
+		return service.updateRating(userName, businessName);
 	}
 }
